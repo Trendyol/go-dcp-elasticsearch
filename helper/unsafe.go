e473192eca4d3a9ac105dd6e09b36d8eb9b683db
mhmtszr <m.szr98@gmail.com>
2023-03-31 21:12:43 +0300

    Add typeName to es config and upgrade Go 1.20

--- helper/unsafe.go
+++ helper/unsafe.go
@@ -1,19 +1,9 @@
 package helper
 
 import (
-	"reflect"
 	"unsafe"
 )
 
-//nolint:unsafeptr
 func Byte(s string) []byte {
-	p := unsafe.Pointer((*reflect.StringHeader)(unsafe.Pointer(&s)).Data)
-
-	var b []byte
-	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	hdr.Data = uintptr(p)
-	hdr.Cap = len(s)
-	hdr.Len = len(s)
-
-	return b
+	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
