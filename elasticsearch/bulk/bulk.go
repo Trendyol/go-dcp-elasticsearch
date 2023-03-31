da95563576be9ab1f65835407fa095682a3f0567
mhmtszr <m.szr98@gmail.com>
2023-03-31 21:39:05 +0300

    Clear github action cache

--- elasticsearch/bulk/bulk.go
+++ elasticsearch/bulk/bulk.go
@@ -56,6 +56,7 @@ func NewBulk(
 		collectionIndexMapping: esConfig.CollectionIndexMapping,
 		typeName:               helper.Byte(esConfig.TypeName),
 	}
+
 	go bulk.StartBulk()
 	return bulk, nil
 }
