4c42a8e71e311e4162644a29b79b79c022aff19b
Eray Arslan <relfishere@gmail.com>
2023-08-10 11:53:29 +0300

    Revert "Fix memory leak"
    This reverts commit 0bc12f05284d3a362116c777f7dfedd48b9bbd96.

--- elasticsearch/bulk/bulk.go
+++ elasticsearch/bulk/bulk.go
@@ -175,6 +175,7 @@ var bytesPool = sync.Pool{
 	New: func() interface{} { return []byte{} },
 }
 
+//nolint:ineffassign
 func getEsActionJSON(
 	docID []byte,
 	action document.EsAction,
@@ -185,9 +186,9 @@ func getEsActionJSON(
 ) []byte {
 	meta := (bytesPool.Get().([]byte))[:0]
 	if action == document.Index {
-		meta = append(meta, indexPrefix...)
+		meta = indexPrefix
 	} else {
-		meta = append(meta, deletePrefix...)
+		meta = deletePrefix
 	}
 	meta = append(meta, helper.Byte(indexName)...)
 	meta = append(meta, idPrefix...)
