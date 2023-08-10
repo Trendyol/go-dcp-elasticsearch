1d674837d35c520111c9deeb1561683e5835d8c3
Eray Arslan <relfishere@gmail.com>
2023-08-10 11:53:25 +0300

    Revert "chore: test fix"
    This reverts commit aff6967c6fb9980a7fee5dfcd735a3c714def9dc.

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -19,7 +19,7 @@ func Mapper(event couchbase.Event) []document.ESActionDocument {
 	return nil
 }
 
-func TestElasticsearch(t *testing.T) {
+func BenchmarkElasticsearch(t *testing.B) {
 	connector, err := dcpelasticsearch.NewConnectorBuilder("config.yml").SetMapper(Mapper).Build()
 	if err != nil {
 		return
