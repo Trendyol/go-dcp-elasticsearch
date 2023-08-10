72bd66e8eaae915890faa05e5b913677646b52ee
Eray Arslan <relfishere@gmail.com>
2023-08-10 11:53:13 +0300

    Revert "chore: remove fasthttp_transport"
    This reverts commit 9b1bc236edb3d11d073c1fcd2124d031506033df.

---
+++ elasticsearch/client/fasthttp_transport.go
@@ -0,0 +1,93 @@
+package client
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/Trendyol/go-dcp-elasticsearch/config"
+	"github.com/Trendyol/go-dcp-elasticsearch/helper"
+	"github.com/valyala/fasthttp"
+)
+
+// transport implements the elastictransport interface with
+// the github.com/valyala/fasthttp HTTP client.
+type transport struct {
+	client *fasthttp.Client
+}
+
+func newTransport(cfg config.Elasticsearch) *transport {
+	client := &fasthttp.Client{
+		MaxConnsPerHost:     fasthttp.DefaultMaxConnsPerHost,
+		MaxIdleConnDuration: fasthttp.DefaultMaxIdleConnDuration,
+	}
+
+	if cfg.MaxConnsPerHost != nil {
+		client.MaxConnsPerHost = *cfg.MaxConnsPerHost
+	}
+
+	if cfg.MaxIdleConnDuration != nil {
+		client.MaxIdleConnDuration = *cfg.MaxIdleConnDuration
+	}
+
+	return &transport{client: client}
+}
+
+// RoundTrip performs the request and returns a response or error
+func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	freq := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(freq)
+
+	fres := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(fres)
+
+	t.copyRequest(freq, req)
+
+	err := t.client.Do(freq, fres)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &http.Response{Header: make(http.Header)}
+	t.copyResponse(res, fres)
+
+	return res, nil
+}
+
+// copyRequest converts a http.Request to fasthttp.Request
+func (t *transport) copyRequest(dst *fasthttp.Request, src *http.Request) *fasthttp.Request {
+	if src.Method == http.MethodGet && src.Body != nil {
+		src.Method = http.MethodPost
+	}
+	dst.SetHost(src.Host)
+	dst.SetRequestURI(src.URL.String())
+	dst.Header.SetRequestURI(src.URL.String())
+	dst.Header.SetMethod(src.Method)
+
+	for k, vv := range src.Header {
+		for _, v := range vv {
+			dst.Header.Set(k, v)
+		}
+	}
+
+	if src.Body != nil {
+		dst.SetBodyStream(src.Body, -1)
+	}
+
+	return dst
+}
+
+// copyResponse converts a http.Response to fasthttp.Response
+func (t *transport) copyResponse(dst *http.Response, src *fasthttp.Response) *http.Response {
+	dst.StatusCode = src.StatusCode()
+
+	src.Header.VisitAll(func(k, v []byte) {
+		dst.Header.Set(helper.String(k), helper.String(v))
+	})
+
+	// Cast to a string to make a copy seeing as src.Body() won't
+	// be valid after the response is released back to the pool (fasthttp.ReleaseResponse).
+	dst.Body = io.NopCloser(strings.NewReader(helper.String(src.Body())))
+
+	return dst
+}
