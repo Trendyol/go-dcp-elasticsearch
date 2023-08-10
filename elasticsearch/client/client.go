9292257684fc7426672bb85499f2fb44931ebd96
Eray Arslan <relfishere@gmail.com>
2023-08-10 11:53:15 +0300

    Revert "feat: remove elastic client"
    This reverts commit 119468b57cd2901d7c7140e843464771408f6195.

--- elasticsearch/client/client.go
+++ elasticsearch/client/client.go
@@ -1,51 +1,22 @@
 package client
 
 import (
-	"errors"
-	"io"
+	"math"
 
 	"github.com/Trendyol/go-dcp-elasticsearch/config"
-	"github.com/valyala/fasthttp"
+
+	"github.com/elastic/go-elasticsearch/v7"
 )
 
-type ElasticClient interface {
-	Bulk(bodyStream io.Reader, bodySize int) ([]byte, error)
-}
-
-type elasticClient struct {
-	url                string
-	compressionEnabled bool
-}
-
-func (e *elasticClient) Bulk(bodyStream io.Reader, bodySize int) ([]byte, error) {
-	req := fasthttp.AcquireRequest()
-	req.SetBodyStream(bodyStream, bodySize)
-	req.Header.SetMethod("POST")
-	req.Header.SetContentType("application/json")
-	req.SetRequestURI(e.url + "/_bulk")
-
-	res := fasthttp.AcquireResponse()
-
-	if err := fasthttp.Do(req, res); err != nil {
+func NewElasticClient(config *config.Config) (*elasticsearch.Client, error) {
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		MaxRetries:          math.MaxInt,
+		Addresses:           config.Elasticsearch.Urls,
+		Transport:           newTransport(config.Elasticsearch),
+		CompressRequestBody: config.Elasticsearch.CompressionEnabled,
+	})
+	if err != nil {
 		return nil, err
 	}
-
-	fasthttp.ReleaseRequest(req)
-
-	body := res.Body()
-
-	fasthttp.ReleaseResponse(res)
-
-	return body, nil
-}
-
-func NewElasticClient(config *config.Config) ElasticClient {
-	if len(config.Elasticsearch.Urls) < 1 {
-		panic(errors.New("invalid elasticsearch urls"))
-	}
-
-	return &elasticClient{
-		url:                config.Elasticsearch.Urls[0],
-		compressionEnabled: config.Elasticsearch.CompressionEnabled,
-	}
+	return es, nil
 }
