90ff8acff767205984d17bacbc67858263520f94
Ferhat Aykan <ferhat.aykan@trendyol.com>
2023-11-21 09:02:53 +0300

    feat: adding cas value to Event

--- couchbase/event.go
+++ couchbase/event.go
@@ -7,34 +7,38 @@ type Event struct {
 	EventTime      time.Time
 	Key            []byte
 	Value          []byte
+	Cas            uint64
 	IsDeleted      bool
 	IsExpired      bool
 	IsMutated      bool
 }
 
-func NewDeleteEvent(key []byte, value []byte, collectionName string) Event {
+func NewDeleteEvent(key []byte, value []byte, collectionName string, cas uint64) Event {
 	return Event{
 		Key:            key,
 		Value:          value,
 		IsDeleted:      true,
 		CollectionName: collectionName,
+		Cas:            cas,
 	}
 }
 
-func NewExpireEvent(key []byte, value []byte, collectionName string) Event {
+func NewExpireEvent(key []byte, value []byte, collectionName string, cas uint64) Event {
 	return Event{
 		Key:            key,
 		Value:          value,
 		IsExpired:      true,
 		CollectionName: collectionName,
+		Cas:            cas,
 	}
 }
 
-func NewMutateEvent(key []byte, value []byte, collectionName string) Event {
+func NewMutateEvent(key []byte, value []byte, collectionName string, cas uint64) Event {
 	return Event{
 		Key:            key,
 		Value:          value,
 		IsMutated:      true,
 		CollectionName: collectionName,
+		Cas:            cas,
 	}
 }
