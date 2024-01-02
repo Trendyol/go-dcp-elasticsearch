4eb954c2a555d5f8c61bfda1f8a0bb3cc9551cb5
Erkan Erkişi <erkanerkisi@gmail.com>
2024-01-02 17:56:20 +0300

    feat: escape quotes on key. (#55)
    * escaping key

---
+++ helper/escape_test.go
@@ -0,0 +1,29 @@
+package helper
+
+import (
+	"testing"
+)
+
+func TestEscapeQuote(t *testing.T) {
+	input := "12345-999\""
+	byteArr := []byte(input)
+	result := EscapePredefinedBytes(byteArr)
+	if len(result) == 10 || result[len(result)-1] != 34 {
+		t.Error("Expected backslash byte")
+	}
+}
+
+func TestDoNotEscapeQuote(t *testing.T) {
+	input := "12345-999"
+	byteArr := []byte(input)
+
+	result := EscapePredefinedBytes(byteArr)
+	if len(result) != 9 {
+		t.Error("Not expected any change related with length")
+	}
+	for _, b := range result {
+		if b == 34 {
+			t.Error("Not expected any backslash")
+		}
+	}
+}
