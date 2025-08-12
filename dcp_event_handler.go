083f86739e379adff1f827ecdc3f6753ab26aca3
Eray Arslan <relfishere@gmail.com>
2025-08-12 16:00:49 +0300

    chore: qol improvements

--- dcp_event_handler.go
+++ dcp_event_handler.go
@@ -5,7 +5,8 @@ import (
 )
 
 type DcpEventHandler struct {
-	bulk *bulk.Bulk
+	bulk     *bulk.Bulk
+	isFinite bool
 }
 
 func (h *DcpEventHandler) BeforeRebalanceStart() {
@@ -28,6 +29,9 @@ func (h *DcpEventHandler) AfterStreamStart() {
 }
 
 func (h *DcpEventHandler) BeforeStreamStop() {
+	if h.isFinite {
+		return
+	}
 	h.bulk.PrepareStartRebalancing()
 }
 
